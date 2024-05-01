99443c2fa44c01c35e2e2de601b68aa8161027f3
quabwww <josuebuenovargas@gmail.com>
2024-05-01 16:36:06 -0500

    v

--- Rutas/Discord API/emojis.py
+++ Rutas/Discord API/emojis.py
@@ -3,7 +3,7 @@ import requests
 
 router = APIRouter()
 
-def obtener_emoji_paginado(token: str, guild_id: str, page: int):
+def obtener_emoji_paginado(token: str, guild_id: str, page: int, pageult: bool = False):
     url = f"https://discord.com/api/v9/guilds/{guild_id}/emojis"
     headers = {
         "Authorization": f"Bot {token}"
@@ -12,6 +12,10 @@ def obtener_emoji_paginado(token: str, guild_id: str, page: int):
     if response.status_code == 200:
         emojis = response.json()
         total_pages = len(emojis)
+        
+        if pageult:
+            page = total_pages - 1
+        
         if 0 <= page < total_pages:
             emoji_id = emojis[page]["id"]
             emoji_url = f"https://discord.com/api/v9/guilds/{guild_id}/emojis/{emoji_id}"
@@ -21,16 +25,17 @@ def obtener_emoji_paginado(token: str, guild_id: str, page: int):
                 emoji_type = "Animado" if emoji_info.get("animated") else "Emoji Normal"
                 emoji_info["type"] = emoji_type
                 return {"emoji": emoji_info, "pagina": page + 1, "pages_total": total_pages}
+        
         return []
     else:
         return None
 
 @router.get("/api/emojis_get/{guild_id}/")
-def get(guild_id: str, page: int = Query(0, ge=0), token: str = Header(None)):
+def get(guild_id: str, page: int = Query(0, ge=0), pageult: bool = Query(False), token: str = Header(None)):
     if token is None:
         return {"error": "Se requiere el token del bot en el encabezado"}
     
-    emoji_paginado = obtener_emoji_paginado(token, guild_id, page)
+    emoji_paginado = obtener_emoji_paginado(token, guild_id, page, pageult)
     if emoji_paginado is not None:
         return emoji_paginado
     else:
